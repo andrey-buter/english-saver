18c1fb7f450a560df41103b3e1ed4797300187f2
Andrei Butser <andrei_butser@epam.com>
2020-06-07 15:42:04 +0300

    update test

--- src/app/services/node-css-path/node-css-path.service.test.ts
+++ src/app/services/node-css-path/node-css-path.service.test.ts
@@ -1,4 +1,4 @@
-import { SelectWord, SaveNodePath } from "./select-word.service";
+import { NodeCssPath } from "./node-css-path.service";
 
 describe('Test selection in single Node', () => {
 	const text = 'Some interesting text';
@@ -7,11 +7,11 @@ describe('Test selection in single Node', () => {
 	const endPosition = startPosition + word.length;
 	// let select;
 
-	let nodePath: SaveNodePath;
+	let nodePath: NodeCssPath;
 
 	beforeAll(() => {
 		// select = new SelectWord();
-		nodePath = new SaveNodePath();
+		nodePath = new NodeCssPath();
 	});
 
 	it('should return correct start position', () => {
@@ -33,12 +33,14 @@ describe('Test selection in single Node', () => {
 		const div = document.createElement('div');
 		div.textContent = text;
 
+		// @ts-ignore
 		expect(nodePath.hasTextNote(div)).toBe(true);
 	});
 
 	it('should check Html Elements has NOT a text node', () => {
 		const div = document.createElement('div');
 
+		// @ts-ignore
 		expect(nodePath.hasTextNote(div)).toBe(false);
 	});
 
@@ -46,6 +48,7 @@ describe('Test selection in single Node', () => {
 		const div = document.createElement('div');
 		div.textContent = '';
 
+		// @ts-ignore
 		expect(nodePath.hasTextNote(div)).toBe(false);
 	});
 
@@ -54,6 +57,7 @@ describe('Test selection in single Node', () => {
 		div.textContent = text;
 		const textNode = div.childNodes[0];
 
+		// @ts-ignore
 		expect(nodePath.isBlockParentNode(textNode)).toBe(true);
 	});
 
@@ -62,6 +66,7 @@ describe('Test selection in single Node', () => {
 		span.textContent = text;
 		const textNode = span.childNodes[0];
 
+		// @ts-ignore
 		expect(nodePath.isBlockParentNode(textNode)).toBe(false);
 	});
 
@@ -70,6 +75,7 @@ describe('Test selection in single Node', () => {
 		const oneClass = 'one-class';
 		div.className = oneClass;
 
+		// @ts-ignore
 		expect(nodePath.getElementClassCssSelector(div)).toBe(`.${oneClass}`);
 	});
 
@@ -77,6 +83,7 @@ describe('Test selection in single Node', () => {
 		const div = document.createElement('div');
 		div.className = 'one-class two-class';
 
+		// @ts-ignore
 		expect(nodePath.getElementClassCssSelector(div)).toBe(`.one-class.two-class`);
 	});
 
@@ -84,20 +91,109 @@ describe('Test selection in single Node', () => {
 		const div = document.createElement('div');
 		div.id = 'id-selector';
 
+		// @ts-ignore
 		expect(nodePath.getElementCssSelector(div)).toBe('#id-selector');
 	});
 
 	it('should return element\s tagName', () => {
 		const div = document.createElement('div');
 
+		// @ts-ignore
 		expect(nodePath.getElementCssSelector(div)).toBe('div');
 	});
 
-	// it('should check that TagName is element selector', () => {
+	// ? TODO: implement a case to calc tagName/className number 
+	// ! or in the case check then by text
+	it('should hierarchy css selector', () => {
+		const selectors = ['div', '.class', '#id'];
 
-	// });
+		// @ts-ignore
+		const result = nodePath.getHierarchyCssSelector(selectors);
 
-	// <div>Some interesting text</div>
-	// interesting
+		expect(result).toBe('#id .class div');
+	});
 
+	it('should return css selector of 3 parents', () => {
+		const tagName = 'textTag';
+
+		document.body.innerHTML = `
+			<div>
+				<div>
+					<div>
+						<p ${tagName}>
+							some text
+						</p>
+					</div>
+				</div>
+			</div>
+		`;
+
+		const textNode = document.querySelector(`[${tagName}]`)?.childNodes[0];
+
+		if (textNode) {
+			const result = nodePath.getParentCssSelector(textNode, 3);
+
+			expect(result).toBe('div div p');
+		}
+	});
+
+	it('should return css selector if first parent has ID', () => {
+		const tagName = 'textTag';
+		const id = 'test-id';
+
+		document.body.innerHTML = `
+			<div>
+				<div>
+					<div>
+						<p id="${id}" ${tagName}>
+							some text
+						</p>
+					</div>
+				</div>
+			</div>
+		`;
+
+		const textNode = document.querySelector('[textTag]')?.childNodes[0];
+
+		if (textNode) {
+			const result = nodePath.getParentCssSelector(textNode, 3);
+
+			expect(result).toBe(`#${id}`);
+		}
+	});
+
+	it('should return css selector if second parent has ID', () => {
+		const tagName = 'textTag';
+		const id = 'test-id';
+		const className = 'test-class';
+
+		document.body.innerHTML = `
+			<div>
+				<div>
+					<div id="${id}">
+						<span class="${className}" ${tagName}>
+							some text
+						</span>
+					</div>
+				</div>
+			</div>
+		`;
+
+		const textNode = document.querySelector('[textTag]')?.childNodes[0];
+
+		if (textNode) {
+			const result = nodePath.getParentCssSelector(textNode, 3);
+
+			expect(result).toBe(`#${id} .${className}`);
+		}
+	});
+
+	it('should return index of a current text node', () => {
+		const tagName = 'textTag';
+
+		document.body.innerHTML = `<div ${tagName}>selection</div>`;
+		document.body.innerHTML = `<span ${tagName}>selection</span>`;
+		document.body.innerHTML = `<div><span ${tagName}>selection</span></div>`;
+		document.body.innerHTML = `<div>Some text <a>link</a> then text <span ${tagName}>selection</span></div>`;
+	})
 });
---
+++ src/app/services/node-css-path/node-css-path.service.ts
@@ -0,0 +1,67 @@
+
+export class NodeCssPath {
+	#blockNodes = /^(div|li|p|body)$/ ; // start with (^) and end ($)
+
+	// startSelectionTextNodeNumber
+	// endSelectionTextNodeNumber
+	// startOffset
+	// endOffset
+	// parent selector
+
+	
+
+	getParentCssSelector(node: Node, depth = 3, cssSelectors: string[] = []): string {
+		const parent = this.getParentElement(node);
+		const selector = this.getElementCssSelector(parent);
+
+		cssSelectors.push(selector);
+
+		if (parent.id) {
+			return this.getHierarchyCssSelector(cssSelectors);
+		}
+
+		depth--;
+
+		if (0 === depth) {
+			return this.getHierarchyCssSelector(cssSelectors);
+		}
+
+		return this.getParentCssSelector(parent, depth, cssSelectors);
+	}
+
+	private hasTextNote(element: HTMLElement) {
+		return element.childNodes.length > 0;
+	}
+
+	private isBlockParentNode(textNode: Node) {
+		return this.#blockNodes.test(this.getParentElement(textNode).tagName.toLowerCase() || 'noop');
+	}
+
+	private getElementCssSelector(element: HTMLElement): string {
+		return this.getElementIdCssSelector(element) || this.getElementClassCssSelector(element) || element.tagName.toLowerCase();
+	}
+
+	private getElementIdCssSelector(element: HTMLElement): string | null {
+		return element.id ? `#${element.id}` : null;
+	}
+
+	private getElementClassCssSelector(element: HTMLElement): string | null {
+		const classes = element.className;
+
+		return classes ? `.${classes.split(' ').join('.')}` : null;
+	}
+
+	// selector[0] - child
+	// selector[1] - parent
+	// expected queue: 'parent > child'
+	private getHierarchyCssSelector(selectors: string[]): string {
+		return selectors.reverse().join(' ');
+	}
+	
+	// <html> doesn't have parent element
+	// so to avoid checking of NULL every where
+	// I will return body element if parentElement doesn't exist
+	private getParentElement(node: Node): HTMLElement {
+		return node.parentElement || document.body;
+	}
+}
