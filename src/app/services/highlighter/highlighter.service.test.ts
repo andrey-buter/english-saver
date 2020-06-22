d722aabdc2e9cbbee2694d1e9b7b31b0c6dd8f40
Andrew Buter <andrey.buter@gmail.com>
2020-06-23 01:00:52 +0300

    upd highlighter test and code the first step of searhing by path

--- src/app/services/highlighter/highlighter.service.test.ts
+++ src/app/services/highlighter/highlighter.service.test.ts
@@ -8,18 +8,143 @@ describe('[Highlighter]', () => {
 		highlighter = new Highlighter();
 	})
 
-	it('should find text node in parent tag with single text node', () => {
-		const path = [
+	it('should find textNode in parent tag with single text node', () => {
+		const text = 'Selection';
+		const path: NodeNumberInParent[] = [
 			{
 				parentTag: 'div',
 				number: 0,
 			},
-		] as NodeNumberInParent[];
+		];
 
-		document
+		document.body.innerHTML = `<div>${text}<span>some text</span></div>`;
 
-		const result = highlighter.findNode(path);
+		const result = highlighter.findTextNode(path);
 
-		expect(result).toBe()
+		expect(result).toBe(text);
 	});
+
+	it('should find textNode in parent tag with single text node', () => {
+		const text = 'Selection';
+		const path: NodeNumberInParent[] = [
+			{
+				parentTag: 'div',
+				number: 2,
+			},
+		];
+
+		document.body.innerHTML = `<div>Some text<span>some text</span>${text}</div>`;
+
+
+		const result = highlighter.findTextNode(path);
+
+		expect(result).toBe(text);
+	});
+
+	it('should find textNode in child tag ', () => {
+		const text = 'Selection';
+		const path: NodeNumberInParent[] = [
+			{
+				parentTag: 'span',
+				number: 3,
+			},
+			{
+				parentTag: 'div',
+				number: 0,
+			},
+		];
+
+		document.body.innerHTML = `<div>Some text<span>some text</span>other text <span>${text}</span></div>`;
+
+		const result = highlighter.findTextNode(path);
+
+		expect(result).toBe(text);
+	});
+
+	it('should find textNode if there is several the same parents', () => {
+		const text = 'Selection';
+		const path: NodeNumberInParent[] = [
+			{
+				parentTag: 'span',
+				number: 0,
+			},
+			{
+				parentTag: 'div',
+				number: 0,
+			},
+		];
+
+		document.body.innerHTML = `<div>some text</div><div><span>${text}</span></div>`;
+
+		const result = highlighter.findTextNode(path);
+
+		expect(result).toBe(text);
+	});
+
+	it('should find textNode in nested parent parent tag', () => {
+		const text = 'Selection';
+		const path: NodeNumberInParent[] = [
+			{
+				parentTag: 'p',
+				number: 0,
+			},
+		];
+
+		document.body.innerHTML = `<div><p>${text}<span>some text</span></p></div>`;
+
+		const result = highlighter.findTextNode(path);
+
+		expect(result).toBe(text);
+	});
+
+	it('should find textNode in nested child element', () => {
+		const text = 'Selection';
+		const path: NodeNumberInParent[] = [
+			{
+				parentTag: 'span',
+				number: 3,
+			},
+			{
+				parentTag: 'b',
+				number: 1,
+			},
+			{
+				parentTag: 'div',
+				number: 0,
+			},
+		];
+
+		document.body.innerHTML = `<div>some text<b>inner text<span>${text}</span></b></div>`;
+
+		const result = highlighter.findTextNode(path);
+
+		expect(result).toBe(text);
+	});
+
+	it('should trigger an error if path doesn\'t find', () => {
+		const text = 'Selection';
+		const path: NodeNumberInParent[] = [
+			{
+				parentTag: 'span',
+				number: 0,
+			},
+			{
+				parentTag: 'b',
+				number: 1,
+			},
+			{
+				parentTag: 'div',
+				number: 3,
+			},
+		];
+
+		document.body.innerHTML = `<div>some text<b>inner text</b></div>`;
+
+		const result = highlighter.findTextNode.bind(highlighter, path);
+
+		//should be error
+		expect(result).toThrow(Error);
+	});
+
+
 });
--- src/app/services/highlighter/highlighter.service.ts
+++ src/app/services/highlighter/highlighter.service.ts
@@ -1,3 +1,4 @@
+import { NodeNumberInParent } from "../../models/node-number-in-parent.model";
 
 export class Highlighter {
 	constructor() {
@@ -15,5 +16,43 @@ export class Highlighter {
 	findSelectedRange() {
 		const elements = this.queryNodes(selector);
 
+	}
+
+	findTextNode(path: NodeNumberInParent[]) {
+		path.reverse().forEach((selector, key) => {
+			let results = [];
+			const elements = document.querySelectorAll(selector.parentTag);
+			const number = selector.number;
+			const index = path.length - key - 1;
+
+			if (!elements.length) {
+				throw new Error(`[Highlighter.findTextNode] Selector doesn't exist: ${JSON.stringify(selector)}`);
+			}
+
+			elements.forEach((element) => {
+				// if element has less children than selector in path
+				if (element.childNodes.length < number) {
+					return;
+				}
+
+				const child = element.childNodes[number];
+
+				// skip text nodes
+				if (index !== 0 && Node.TEXT_NODE === child.nodeType) {
+					return;
+				}
+
+				results.push(child);
+			});
+
+			if (!results.length) {
+				throw new Error(`[Highlighter.findTextNode] Selector doesn't have a child with index: ${JSON.stringify(selector)}`);
+			}
+
+			// я сделал проход только по верхнему родителю
+			// у нас есть results с children elements,
+			// и теперь в каждом элементе!!! нужно делалть такой же поиск
+			// dthjznyj ghbltncz xnj-nj vytnmz d aeyrwbb
+		});
 	}
 }
